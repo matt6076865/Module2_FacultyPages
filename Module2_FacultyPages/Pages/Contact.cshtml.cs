54d08a037377b8e0b73629464ebf5595fa840f87
matt6076865 <matt6076865@gmail.com>
2026-03-28 14:15:14 -0400

    Add project files.

---
+++ Module2_FacultyPages/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Module2_FacultyPages.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Module2_FacultyPages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Module2_FacultyPages.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
